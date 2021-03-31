c4a2f64af0e06a3c5896ef5fdd6d7dc8b51a9895
PL <lewski997@gmail.com>
2021-03-31 09:47:13 +0200

    zrobione na funkcjach v2

--- main.c
+++ main.c
@@ -1,87 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
-float prostokat(float a, float b);
-float trojkat(float h, float pods);
-int main(void){
+#include <conio.h>
 
+
+float pole_prostokata();
+float pole_trojkata();
+
+int main()
+{
 for(;;)
 {
-int c=68 ;
-float a;
-float b;
-float wynik;
-float pods;
-float h;
-float wynik2;
 
-printf("wybierz cos\n");
-printf("1.pole ptostokata\n");
+int c=68 ;
+
+printf("Wybierz figure\n");
+printf("1.pole prostokata\n");
 printf("2.pole trojkata\n");
 printf("3. wyjscie\n");
-scanf("%d", &c);
+c =getch();
 switch (c)
 
 {
 
-case 1:
+case '1':
+pole_prostokata();
+for(int i= 0; i<=2;i++)
+    {
+        printf("\n");
+    };
+break;
 
+case '2':
+pole_trojkata();
+for(int i= 0; i<=2;i++)
+    {
+        printf("\n");
+    };
+break;
+
+case '3':
+exit(EXIT_SUCCESS);
+
+
+default:
+    printf("Zly wybor prosze wybrac cyfre od 1 do 3\n");
+    printf("nacisnij dowolny przycisk zeby wrocic do menu\n");
+    //exit(EXIT_SUCCESS);
+    getch();
+    for(int i= 0; i<=2;i++)
+    {
+        printf("\n");
+    };
+break;
+}
+
+}
+    return 0;
+};
+
+float pole_prostokata()
+{
+    float a, b, wynik;
     printf("wprowadz a\n");
     scanf("%f", &a);
     if (a<=0)
     {
-        printf("wprowadzono zle wartosci, wartosc a musi byc wieksza od 0\n");
-        exit (EXIT_FAILURE);
+         printf("a musi byc wieksze od 0\n");
+         exit (EXIT_FAILURE);
     };
-    printf("wprowadz b\n");
+     printf("wprowadz b\n");
     scanf("%f", &b);
     if (b<=0)
     {
-        printf("wprowadzono zle wartosci, wartosc b musi byc wieksza od 0\n");
-        return 0;
-        exit(EXIT_FAILURE);
-
+         printf("b musi byc wieksze od 0\n");
+         exit (EXIT_FAILURE);
     };
+    wynik = a*b;
+    printf("wynik= ""%.4f", wynik);
+    return wynik;
+};
 
-   prostokat(a, b);/////////////////////////////Funkcja obliczaj¹ca pole prostok¹ta
-    break;
-case 2:
 
-    printf("wprowadz podstawe trojkata\n");
-    scanf("%f", &pods);
-    if (pods<=0)
+float pole_trojkata()
+{
+    float a, h, wynik;
+    printf("wprowadz a\n");
+    scanf("%f", &a);
+    if (a<=0)
     {
-        printf("wprowadzono zle wartosci, wartosc podstawy musi byc wieksza od 0\n");
-        exit (EXIT_FAILURE);
+         printf("a musi byc wieksze od 0\n");
+         exit (EXIT_FAILURE);
     };
-    printf("wprowadz wysokosc trojkata\n");
+     printf("wprowadz h\n");
     scanf("%f", &h);
     if (h<=0)
     {
-        printf("wprowadzono zle wartosci, wartosc h musi byc wieksza od 0\n");
-        exit (EXIT_FAILURE);
+         printf("h musi byc wieksze od 0\n");
+         exit (EXIT_FAILURE);
     };
-    trojkat(h, pods);/////////////////////////////////////Funkcaj obliczaj¹ca pole trojkata
-    break;
-
-case 3:
-    exit;
-    return 0;
-}
-}
-};
-
-float prostokat(float a, float b)
-{
-    float wynik;
-     wynik = a*b;
-     printf("wynik=" "%f\n", wynik);
-     return wynik;
-};
-float trojkat(float h, float pods)
-{
-    float wynik;
-    wynik = 0.5*pods*h;
-    printf("wynik=" "%f\n", wynik);
+    wynik = (a*h)/2.0;
+    printf("wynik= ""%.4f", wynik, "\n");
     return wynik;
 };
 
